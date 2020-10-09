c0eda6fc0e5da5cd89b862ac3562c5329a7d979e
Deepanshu <deepanshudhingra01@gmail.com>
2020-10-10 03:29:30 +0530

    changes

--- client/public/contact.js
+++ client/public/contact.js
@@ -1,7 +1,20 @@
 let contact_button = document.getElementsByClassName("button_new")[0]
 
-contact_button.addEventListener("click",async (e) => {
-    e.preventDefault();
+async function contact_details(){
+    // e.preventDefault();
     const contact_data = await axios.get('https://api.rootnet.in/covid19-in/contacts')
-    
-})
+    var i;
+    var table=document.getElementsByClassName('table-fill')[0];
+    var tbody=document.getElementsByClassName('table-hover')[0].innerHTML="";
+    console.log(contact_data.data.data.contacts.regional)
+    for(i=0;i<contact_data.data.data.contacts.regional.length;i++)
+    {
+        var loc="<td>"+contact_data.data.data.contacts.regional[i].loc+"</td>";
+        var num="<td>"+contact_data.data.data.contacts.regional[i].number+"</td>";
+        var fdata=loc+num;
+        var ffdata="<tr>"+fdata+"</tr>";
+        $(table).find('tbody').append(ffdata);
+
+    }
+    console.log("Heyyaaaa")
+}
