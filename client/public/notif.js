4bdfd17fc54a155cbe64c4e4527ecb448f5da993
Naman Agarwal <namana019@outlook.com>
2020-10-10 20:15:10 +0530

    Notif page

--- client/public/notif.js
+++ client/public/notif.js
@@ -1,6 +1,18 @@
 let notif_button = document.getElementsByClassName("button_new")[0]
-notif_button.addEventListener("click",async (e) => {
-    e.preventDefault();
-    const notif_data = await axios.get('https://api.rootnet.in/covid19-in/notifications')
-    console.log(notif_data)
-})
+
+async function contact_details(){
+    const contact_data = await axios.get('https://api.rootnet.in/covid19-in/notifications')
+    var i;
+    var table=document.getElementsByClassName('table-fill')[0];
+    var tbody=document.getElementsByClassName('table-hover')[0].innerHTML="";
+    console.log(contact_data.data.data.notifications)
+    for(i=0;i<contact_data.data.data.notifications.length;i++)
+    {
+        var loc="<td>"+contact_data.data.data.notifications[i].title+"</td>";
+        var num="<td class='contact-link'>"+contact_data.data.data.notifications[i].link+"</td>";
+        var fdata=loc+num;
+        var ffdata="<tr>"+fdata+"</tr>";
+        $(table).find('tbody').append(ffdata);
+
+    }
+}
